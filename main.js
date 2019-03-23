9d51ad80829d0ed63161b088d72306e341e16632
Tom 'RampantEpsilon' Ware <tomjware@gmail.com>
2019-03-23 09:58:38 -0500

    Initial Commit

---
+++ main.js
@@ -0,0 +1,287 @@
+const {BrowserWindow, Menu, app, shell, dialog2} = require('electron')
+
+//App Info
+const label = 'SMO Any% Moon Randomizer'
+const build = '2019.3.23'
+const version = app.getVersion()
+const title = "SMO Any% Moon Randomizer v1.0.0-beta"
+
+//Application Menu
+let template = [{
+  label: 'View',
+  submenu: [{
+    label: 'Reload',
+    accelerator: 'CmdOrCtrl+R',
+    click: (item, focusedWindow) => {
+      if (focusedWindow) {
+        // on reload, start fresh and close any old
+        // open secondary windows
+        if (focusedWindow.id === 1) {
+          BrowserWindow.getAllWindows().forEach(win => {
+            if (win.id > 1) win.close()
+          })
+        }
+        focusedWindow.reload()
+      }
+    }
+  }, {
+    label: 'Toggle Full Screen',
+    accelerator: (() => {
+      if (process.platform === 'darwin') {
+        return 'Ctrl+Command+F'
+      } else {
+        return 'F11'
+      }
+    })(),
+    click: (item, focusedWindow) => {
+      if (focusedWindow) {
+        focusedWindow.setFullScreen(!focusedWindow.isFullScreen())
+      }
+    }
+  }, {
+    type: 'separator'
+  }]
+}, {
+  label: 'Window',
+  role: 'window',
+  submenu: [{
+    label: 'Minimize',
+    accelerator: 'CmdOrCtrl+M',
+    role: 'minimize'
+  }, {
+    label: 'Close',
+    accelerator: 'CmdOrCtrl+W',
+    role: 'close'
+  }, {
+    type: 'separator'
+  }]
+}, {
+  label: 'About',
+  role: 'about',
+  submenu: [{
+    label: label,
+    enabled: false,
+    },{
+    label: 'Version ' + version,
+    enabled: false,
+    },{
+    label: 'Build: ' + build,
+    enabled: false,
+    },{
+    label: 'Author: ' + 'Tom "RampantEpsilon" Ware',
+    enabled: false,
+    }]
+}]
+
+function addUpdateMenuItems (items, position) {
+  if (process.mas) return
+
+  const version = app.getVersion()
+  let updateItems = [/*{
+    label: `Version ${version}`,
+    enabled: false
+  }, {
+    label: 'Checking for Update',
+    enabled: false,
+    key: 'checkingForUpdate'
+  }, {
+    label: 'Check for Update',
+    visible: false,
+    key: 'checkForUpdate',
+    click: () => {
+      require('electron').autoUpdater.checkForUpdates()
+    }
+  }, {
+    label: 'Restart and Install Update',
+    enabled: true,
+    visible: false,
+    key: 'restartToUpdate',
+    click: () => {
+      require('electron').autoUpdater.quitAndInstall()
+    }
+  }*/]
+
+  items.splice.apply(items, [position, 0].concat(updateItems))
+}
+
+function findReopenMenuItem () {
+  const menu = Menu.getApplicationMenu()
+  if (!menu) return
+
+  let reopenMenuItem
+  menu.items.forEach(item => {
+    if (item.submenu) {
+      item.submenu.items.forEach(item => {
+        if (item.key === 'reopenMenuItem') {
+          reopenMenuItem = item
+        }
+      })
+    }
+  })
+  return reopenMenuItem
+}
+
+if (process.platform === 'darwin') {
+  const name = app.getName()
+  template.unshift({
+    label: name,
+    submenu: [{
+      label: `About ${name}`,
+      role: 'about'
+    }, {
+      type: 'separator'
+    }, {
+      label: 'Services',
+      role: 'services',
+      submenu: []
+    }, {
+      type: 'separator'
+    }, {
+      label: `Hide ${name}`,
+      accelerator: 'Command+H',
+      role: 'hide'
+    }, {
+      label: 'Hide Others',
+      accelerator: 'Command+Alt+H',
+      role: 'hideothers'
+    }, {
+      label: 'Show All',
+      role: 'unhide'
+    }, {
+      type: 'separator'
+    }, {
+      label: 'Quit',
+      accelerator: 'Command+Q',
+      click: () => {
+        app.quit()
+      }
+    }]
+  })
+
+  // Window menu.
+  template[3].submenu.push({
+    type: 'separator'
+  }, {
+    label: 'Bring All to Front',
+    role: 'front'
+  })
+
+  addUpdateMenuItems(template[0].submenu, 1)
+}
+
+if (process.platform === 'win32') {
+  const helpMenu = template[template.length - 1].submenu
+  addUpdateMenuItems(helpMenu, 3)
+}
+
+app.on('ready', () => {
+  const menu = Menu.buildFromTemplate(template)
+  Menu.setApplicationMenu(menu)
+})
+
+app.on('browser-window-created', () => {
+  let reopenMenuItem = findReopenMenuItem()
+  if (reopenMenuItem) reopenMenuItem.enabled = false
+})
+
+app.on('window-all-closed', () => {
+  let reopenMenuItem = findReopenMenuItem()
+  if (reopenMenuItem) reopenMenuItem.enabled = true
+})
+
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let win
+
+function createWindow () {
+  const mainWindow = new BrowserWindow({
+    width: 480,
+    height: 720,
+    icon: __dirname + '/Icon.png',
+    title: title,
+    webPreferences: {
+      nativeWindowOpen: true
+    }
+  })
+  mainWindow.webContents.on('new-window', (event, url, frameName, disposition, options, additionalFeatures) => {
+    if (frameName === 'modal') {
+      // open window as modal
+      event.preventDefault()
+      Object.assign(options, {
+        modal: true,
+        parent: mainWindow,
+        width: 1280,
+        height: 720
+      })
+      event.newGuest = new BrowserWindow(options)
+    }
+    else if (frameName === 'twitter') {
+      event.preventDefault()
+      Object.assign(options, {
+        modal: false,
+        parent: mainWindow,
+        width: 200,
+        height: 720
+      })
+    }
+  })
+  // Create the browser window.
+  //win = new BrowserWindow({ width: 1280, height: 720, icon: __dirname + '/tgsn.png' })
+
+  // and load the index.html of the app.
+  mainWindow.loadFile('src/index.html')
+
+  // Open the DevTools.
+  //win.webContents.openDevTools()
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', () => {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    //win = null
+  })
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow)
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (win === null) {
+    createWindow()
+  }
+})
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
+
+
+
+//Dialog Box
+const {ipcMain, dialog} = require('electron')
+
+ipcMain.on('open-information-dialog', (event) => {
+  const options = {
+    type: 'info',
+    title: 'Information',
+    message: 'messageText',
+    buttons: ['Yes', 'No']
+  }
+  dialog.showMessageBox(options, (index) => {
+    event.sender.send('information-dialog-selection', index)
+  })
+})
---
+++ src/scripts/any.js
@@ -0,0 +1,228 @@
+
+  var seed = [];
+  var moonlist = "";
+  var hash = "null";
+  var check = "<input type='checkbox'>";
+
+  function clear(){
+    document.getElementById("seedHash").value = "";
+  };
+
+  function copySeed(){
+    var copyText = document.getElementById("seedHash");
+    copyText.select();
+    document.execCommand("copy");
+  };
+
+  function pasteSeed(){
+    var pasteText = document.getElementById("seedHash");
+    pasteText.select();
+    document.execCommand("paste");
+    generateSeed();
+  }
+
+  function generateSeed(){
+      moonlist = "<br>"
+      hash = document.getElementById("seedHash").value;
+
+      //Setup Lists
+      var cascadeList = "<br><b><u>Cascade Kingdom</u></b><br><input type='checkbox'>" + myObj.cascade[15] + "<br><input type='checkbox'>" + myObj.cascade[16] + "<br>";
+      var sandList = "<br><b><u>Sand Kingdom</u></b><br>";
+      var lakeList = "<br><b><u>Lake Kingdom</u></b><br><input type='checkbox'>" + myObj.lake[17] + "<br>";
+      var woodList = "<br><b><u>Wooded Kingdom</u></b><br><input type='checkbox'>" + myObj.wooded[15] + "<br><input type='checkbox'>" + myObj.wooded[16] + "<br>";
+      var lostList = "<br><b><u>Lost Kingdom</u></b><br>";
+      var metroList = "<br><b><u>Metro Kingdom</u></b><br><input type='checkbox'>" + myObj.metro[35] + "<br>";
+      var snowList = "<br><b><u>Snow Kingdom</u></b><br>";
+      var seasideList = "<br><b><u>Seaside Kingdom</u></b><br>";
+      var luncheonList = "<br><b><u>Luncheon Kingdom</u></b><br>" + check + myObj.luncheon[29] + "<br>" + check + myObj.luncheon[30] + "<br>" + check + myObj.luncheon[31] + "<br>";
+      var ruinedList = "<br><b><u>Ruined Kingdom</u></b></br>" + check + myObj.ruined[0] + "<br>";
+      var bowserList = "<br><b><u>Bowser's Kingdom</u></b><br>" + check + myObj.bowser[23] + "<br>" + check + myObj.bowser[24] + "<br>" + check + myObj.bowser[25] + "<br>" + check + myObj.bowser[26] + "<br>";
+
+      //Determine Moons Based On Hash/Randomize
+      if (hash == ''){
+        //Cascade Randomizer
+        for (a=0; a < 1; a++){
+          var aa = Math.floor(Math.random() * 15);
+          cascadeList = cascadeList + check + myObj.cascade[aa] + "<br>";
+          var aSeed = aa;
+          seed[0] = aSeed.toString(36);
+        }
+
+        //Sand Randomizer
+        var sandArr = [];
+        while(sandArr.length < 16){
+          var ba = Math.floor(Math.random() * 32);
+          if (sandArr.indexOf(ba) === -1) sandArr.push(ba);
+        }
+        for (bb=0; bb < 16; bb++){
+          sandList = sandList + check + myObj.sand[sandArr[bb]] + "<br>";
+        }
+        for (bc=0; bc < 16; bc++){
+          var bd = bc+1;
+          seed[bd] = (sandArr[bc]).toString(36);
+        }
+
+        //Lake Randomizer
+        var lakeArr = [];
+        while(lakeArr.length < 5){
+          var ca = Math.floor(Math.random() * 17);
+          if (lakeArr.indexOf(ca) === -1) lakeArr.push(ca);
+        }
+        for (c = 0; c < 5; c++){
+          lakeList = lakeList + check + myObj.lake[lakeArr[c]] + "<br>";
+        }
+        for (cc = 0; cc < 5; cc++){
+          var cd = cc+17;
+          seed[cd] = (lakeArr[cc]).toString(36);
+        }
+
+        //Wooded Randomizer
+        var woodArr = [];
+        while(woodArr.length < 12){
+          var da = Math.floor(Math.random() * 15);
+          if (woodArr.indexOf(da) === -1) woodArr.push(da);
+        }
+        for (d = 0; d < 12; d++){
+          woodList = woodList + check + myObj.wooded[woodArr[d]] + "<br>";
+        }
+        for (dc = 0; dc < 12; dc++){
+          var dd = dc+22;
+          seed[dd] = (woodArr[dc]).toString(36);
+        }
+
+        //Lost Randomizer
+        var lostArr = [];
+        while(lostArr.length < 10){
+          var ea = Math.floor(Math.random() * 20);
+          if (lostArr.indexOf(ea) === -1) lostArr.push(ea);
+        }
+        for (e = 0; e < 10; e++){
+          lostList = lostList + check + myObj.lost[lostArr[e]] + "<br>";
+        }
+        for (ec = 0; ec < 10; ec++){
+          var ed = ec+34;
+          seed[ed] = (lostArr[ec]).toString(36);
+        }
+
+        //Metro Randomizer
+        var metroArr = [];
+        while(metroArr.length < 17){
+          var fa = Math.floor(Math.random() * 35);
+          if (metroArr.indexOf(fa) === -1) metroArr.push(fa);
+        }
+        for (f = 0; f < 17; f++){
+          metroList = metroList + check + myObj.metro[metroArr[f]] + "<br>";
+        }
+        for (fc = 0; fc < 17; fc++){
+          var fd = fc+44;
+          seed[fd] = (metroArr[fc]).toString(36);
+        }
+
+        //Snow Randomizer
+        var snowArr = [];
+        while(snowArr.length < 10){
+          var ga = Math.floor(Math.random() * 13);
+          if (snowArr.indexOf(ga) === -1) snowArr.push(ga);
+        }
+        for (g = 0; g < 10; g++){
+          snowList += check + myObj.snow[snowArr[g]] + "<br>";
+        }
+        for (gc = 0; gc < 10; gc++){
+          var gd = gc+61;
+          seed[gd] = (snowArr[gc]).toString(36);
+        }
+
+        //Seaside Randomizer
+        var seasideArr = [];
+        while(seasideArr.length < 10){
+          var ha = Math.floor(Math.random() * 36);
+          if (seasideArr.indexOf(ha) === -1) seasideArr.push(ha);
+        }
+        for (h = 0; h < 10; h++){
+          seasideList += check + myObj.seaside[seasideArr[h]] + "<br>";
+        }
+        for (hc = 0; hc < 10; hc++){
+          var hd = hc+71;
+          seed[hd] = (seasideArr[hc]).toString(36);
+        }
+
+        //Luncheon Randomizer
+        var luncheonArr = [];
+        while(luncheonArr.length < 13){
+          var ia = Math.floor(Math.random() * 29);
+          if (luncheonArr.indexOf(ia) === -1) luncheonArr.push(ia);
+        }
+        for (i = 0; i < 13; i++){
+          luncheonList += check + myObj.luncheon[luncheonArr[i]] + "<br>";
+        }
+        for (ic = 0; ic < 13; ic++){
+          var id = ic+81;
+          seed[id] = (luncheonArr[ic]).toString(36);
+        }
+
+        //Bowser Randomizer
+        var bowserArr = [];
+        while(bowserArr.length < 2){
+          var ja = Math.floor(Math.random() * 27);
+          if (bowserArr.indexOf(ja) === -1) bowserArr.push(ja);
+        }
+        for (j = 0; j < 2; j++){
+          bowserList += check + myObj.bowser[bowserArr[j]] + "<br>";
+        }
+        for (jc = 0; jc < 2; jc++){
+          var jd = jc + 94;
+          seed[jd] = (bowserArr[jc]).toString(36);
+        }
+
+        //Add Lists to moonlist
+        moonlist += cascadeList + sandList + lakeList + woodList + lostList + metroList + snowList + seasideList + luncheonList + ruinedList + bowserList;
+
+        document.getElementById("moons").innerHTML = moonlist + "<br><br>";
+        //document.getElementById('seed').innerHTML = hash;
+        document.getElementById('seedHash').value = seed.join("").toUpperCase();
+      }
+      else{
+        //Decode Hash
+        var moons = hash.split("");
+        for (i = 0; i < moons.length; i++){
+          moons[i] = parseInt(moons[i], 36);
+        }
+
+        //Add Moons to Lists
+        cascadeList += check + myObj.cascade[moons[0]] + "<br>";
+        for (az = 1; az < 17; az++){
+          sandList += check + myObj.sand[moons[az]] + "<br>";
+        }
+        for (bz = 17; bz < 22; bz++){
+          lakeList += check + myObj.lake[moons[bz]] + "<br>";
+        }
+        for (cz = 22; cz < 34; cz++){
+          woodList += check + myObj.wooded[moons[cz]] + "<br>";
+        }
+        for (dz = 34; dz < 44; dz++){
+          lostList += check + myObj.lost[moons[dz]] + "<br>";
+        }
+        for (ez = 44; ez < 61; ez++){
+          metroList += check + myObj.metro[moons[ez]] + "<br>";
+        }
+        for (fz = 61; fz < 71; fz++){
+          snowList += check + myObj.snow[moons[fz]] + "<br>";
+        }
+        for (gz = 71; gz < 81; gz++){
+          seasideList += check + myObj.seaside[moons[gz]] + "<br>";
+        }
+        for (hz = 81; hz < 94; hz++){
+          luncheonList += check + myObj.luncheon[moons[hz]] + "<br>";
+        }
+        for (iz = 94; iz < 96; iz++){
+          bowserList += check + myObj.bowser[moons[iz]] + "<br>";
+        }
+
+        //Add Lists to moonlist
+        moonlist += cascadeList + sandList + lakeList + woodList + lostList + metroList + snowList + seasideList + luncheonList + ruinedList + bowserList;
+
+        document.getElementById("moons").innerHTML = moonlist + "<br>";
+        document.getElementById('seedHash').innerHTML = hash;
+        //document.getElementById('seed').innerHTML = seed.join("").toUpperCase();
+      }
+  };
