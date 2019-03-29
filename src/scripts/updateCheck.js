017421f6749d0f68e426e9215885836173cc9cfd
Tom 'RampantEpsilon' Ware <tomjware@gmail.com>
2019-03-29 01:49:06 -0500

    Cleaned up the update checker

---
+++ src/scripts/updateCheck.js
@@ -0,0 +1,12 @@
+window.$ = window.jQuery = require('jquery');
+
+var currentVer = '1.2.0';
+
+$(document).ready(function(){
+  $.getJSON("https://api.github.com/repos/rampantepsilon/smoanyrando/releases", function(result){
+    var commit = result[0].tag_name;
+    if (commit != currentVer && commit > currentVer){
+      $("#updateCheck").append("New Version (v"+commit+") available! <a href='https://github.com/rampantepsilon/smoanyrando/releases' target='_blank'>Click Here to Download</a>");
+    }
+  });
+});
