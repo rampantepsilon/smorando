987daedf8eead06a81d141c52d487014839c86ae
Tom 'RampantEpsilon' Ware <tomjware@gmail.com>
2019-04-04 12:52:18 -0500

    Updated to exclude the upcoming 1.3.0-alpha and beta releases

--- src/scripts/updateCheck.js
+++ src/scripts/updateCheck.js
@@ -1,11 +1,11 @@
 window.$ = window.jQuery = require('jquery');
 
-var currentVer = '1.2.1';
+var currentVer = '1.2.2';
 
 $(document).ready(function(){
   $.getJSON("https://api.github.com/repos/rampantepsilon/smoanyrando/releases", function(result){
     var commit = result[0].tag_name;
-    if (commit != currentVer && commit > currentVer){
+    if (commit != currentVer && commit > currentVer && commit.contains('alpha' || 'beta') == false){
       $("#updateCheck").append("New Version (v"+commit+") available! <a href='https://github.com/rampantepsilon/smoanyrando/releases' target='_blank'>Click Here to Download</a>");
     }
   });
