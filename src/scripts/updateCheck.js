57add69ca176c276beb1585b849e501312ae4158
Tom 'RampantEpsilon' Ware <tomjware@gmail.com>
2019-04-07 10:48:32 -0500

    Changed update check to directly download updates

--- src/scripts/updateCheck.js
+++ src/scripts/updateCheck.js
@@ -1,12 +1,22 @@
 window.$ = window.jQuery = require('jquery');
 
-var currentVer = '1.3.0-alpha';
+var currentVer = '1.2.2';
 
 $(document).ready(function(){
   $.getJSON("https://api.github.com/repos/rampantepsilon/smoanyrando/releases", function(result){
     var commit = result[0].tag_name;
+    var linUpdate = "New Version (v" + commit + ") available! <a href='https://github.com/rampantepsilon/smorando/releases/download/2.0-alpha/SMO-Moon-Randomizer-v2.0.0-alpha_Linux-x64.zip' target='_blank'>Click Here to Download</a>";
+    var winUpdate = "New Version (v" + commit + ") available! <a href='https://github.com/rampantepsilon/smorando/releases/download/2.0-alpha/SMO-Moon-Randomizer-v2.0.0-alpha_Windows-x64.exe' target='_blank'>Click Here to Download</a>";
     if (commit != currentVer && commit > currentVer/* && commit.contains('alpha' || 'beta') == false*/){
-      $("#updateCheck").append("New Version (v"+commit+") available! <a href='https://github.com/rampantepsilon/smoanyrando/releases' target='_blank'>Click Here to Download</a>");
+      if (process.platform === 'linux'){
+        $("#updateCheck").append(linUpdate);
+      }
+      else if (process.platform === 'win32'){
+        $("#updateCheck").append(winUpdate);
+      }
+      else {
+        $("#updateCheck").append("New Version (v" + commit + ") available! <a href='https://github.com/rampantepsilon/smorando/releases/' target='_blank'>Click Here to Download</a>");
+      }
     }
   });
 });
