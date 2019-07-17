c3219afc48511c0eb298672536bc1b7335bb54f0
Tom 'RampantEpsilon' Ware <tomjware@gmail.com>
2019-07-17 11:09:21 -0500

    Added toggle for dev/full

--- src/scripts/version.js
+++ src/scripts/version.js
@@ -1,6 +1,7 @@
 var version;
 version = {
   'current':'2.1.0',
-  'compat':['210f','210d'],
+  'compat':['210f','210d']
+  //'compat':['210d','210f'],
   'versionSupported':['2.1.0','2.1.0-dev']
 };
