59789c3b8b43283d69d14468881e292304b28109
Tom 'RampantEpsilon' Ware <tomjware@gmail.com>
2019-07-22 20:56:13 -0500

    Reversed 2.2.0 full and dev strings

--- src/scripts/version.js
+++ src/scripts/version.js
@@ -2,7 +2,7 @@ var version;
 version = {
   'current':'2.2.0',
   //'compat':['212f','211f'],
-  'compat':['220d','220f','212f','211f'],
+  'compat':['220f','220d','212f','211f'],
   'versionSupported':['2.1.1','2.1.2','2.2.0-dev'],
   //'versionSupported':['2.1.1','2.1.2']
 };
